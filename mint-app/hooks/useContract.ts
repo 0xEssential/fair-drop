a8f931697bd0d74ad5b8a7e2b9f6eb55430f1cdd
Sam Bauch <sbauch@gmail.com>
2021-11-09 16:01:55 -0500

    ethers

--- mint-app/hooks/useContract.ts
+++ mint-app/hooks/useContract.ts
@@ -1,11 +1,8 @@
 import { Contract, ContractInterface } from '@ethersproject/contracts';
 import { useContext, useMemo } from 'react';
 
-import {
-  FairDropRegistration,
-  NFTStateTransfer,
-} from '../../contracts/typechain';
 import { Web3Context } from '../contexts/web3Context';
+import { FairDropRegistration, NFTStateTransfer } from '../typechain';
 
 export default function useContract<
   T extends FairDropRegistration | NFTStateTransfer,
